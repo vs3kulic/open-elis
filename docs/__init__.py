e808d616db97b2e3b340553508c9c9216f5ff59f
vs3kulic <sekulicvajo@gmail.com>
2025-10-01 08:50:30 +0200

    chore(docs): added package docstrings

---
+++ docs/__init__.py
@@ -0,0 +1,9 @@
+"""
+Documentation package for the Open ELIS therapy recommendation system.
+
+This package contains documentation, including API docs, data base documentation,
+and entity-relationship diagrams.
+
+Author: Vajo Sekulic
+Version: 0.1.0
+"""
--- docs/source/endpoints.root.rst
+++ docs/source/endpoints.root.rst
@@ -1,8 +1,6 @@
 endpoints.root module
 ====================
 
-This module contains the root endpoints for health checks and system status.
-
 .. automodule:: endpoints.root
    :members:
    :undoc-members:
